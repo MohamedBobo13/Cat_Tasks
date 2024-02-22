bf468b2b59a26c4095651e43876727f1e57deb47
Mohamed Bobo <mo7amed.bobo13@gmail.com>
2024-02-22 20:24:38 +0200

    02

---
+++ Week 1/02/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int summation(int *x,int *y){
+    return *x + *y;
+}
+int main(){
+  int x,y;
+  scanf("%d %d",&x,&y);
+  int result = summation(&x,&y);
+    printf("%d\n",result);
+}
